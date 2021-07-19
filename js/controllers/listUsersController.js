792cd468a2fe6c8f36192aab6a12103aff7a9bb6
pierrefrota <pierrefrota@gmail.com>
2021-07-19 18:30:16 -0300

    loading

--- js/controllers/listUsersController.js
+++ js/controllers/listUsersController.js
@@ -1,8 +1,10 @@
 app.controller("listUsersController", function ($scope, $http) {
   var loadUsers = () => {
-    $http
-      .get("http://localhost:3007/users")
-      .then((res) => ($scope.users = res.data));
+    $scope.loading = true;
+    $http.get("http://localhost:3007/users").then((res) => {
+      $scope.users = res.data;
+      $scope.loading = false;
+    });
   };
 
   loadUsers();
