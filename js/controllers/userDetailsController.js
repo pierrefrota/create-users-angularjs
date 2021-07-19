d4019eab05d903eb759e95503b7d420b954b675e
pierrefrota <pierrefrota@gmail.com>
2021-07-19 16:55:51 -0300

    user details

--- js/controllers/userDetailsController.js
+++ js/controllers/userDetailsController.js
@@ -1,5 +1,11 @@
-app.controller("userDetailsController", function ($scope, $routeParams) {
-  var idParam = $routeParams.id;
+app.controller("userDetailsController", function ($scope, $routeParams, $http) {
+  var userId = $routeParams.id;
 
-  console.log(idParam);
+  var loadUserById = () => {
+    $http
+      .get(`http://localhost:3007/users/${userId}`)
+      .then((res) => ($scope.user = res.data));
+  };
+
+  loadUserById();
 });
